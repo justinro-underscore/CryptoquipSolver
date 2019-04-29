9a6327238a02c7b9411800c8a2181b1b653bc491
justinro-underscore <jcroderman@gmail.com>
2019-04-28 20:44:48 -0500

    Adds base functionality

--- src/main/CryptoquipUI.java
+++ src/main/CryptoquipUI.java
@@ -1,14 +1,19 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import javafx.application.Application;
-import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /**
@@ -21,8 +26,12 @@ import javafx.stage.Stage;
 public class CryptoquipUI extends Application
 {
 	private final int MAX_LETTERS = 26;
+	private final int MAX_LABEL_LENGTH = 20;
 
 	@FXML private GridPane grdStrings;
+
+	@FXML private Label[] codeLabels;
+	@FXML private Label[] translatedLabels;
 
 	@FXML private TextField txtA;
 	@FXML private TextField txtB;
@@ -50,10 +59,9 @@ public class CryptoquipUI extends Application
 	@FXML private TextField txtX;
 	@FXML private TextField txtY;
 	@FXML private TextField txtZ;
+	@FXML private TextField[] textFields;
 
-	@FXML private Button btn;
-
-	private TextField[] textFields;
+	private String solutionStr;
 	private char[] charTranslations;
 
 	/**
@@ -83,9 +91,11 @@ public class CryptoquipUI extends Application
 		stage.setTitle("Cryptoquip Solver");
 		stage.setScene(scene);
 		initUI();
-		setPuzzle("This is a test");
 
 		stage.show();
+
+		// TODO Move this
+		setPuzzle("FVQS HGWBC RGW HQBB SVK GUKJVKQC ZGMKR MKKCKC SG ZQTMSQTM Q ZGUTME OSQTJFQR? KOHQBQSTME HGOSO.");
 	}
 
 	/**
@@ -146,10 +156,6 @@ public class CryptoquipUI extends Application
 		{
 			initTextFieldListener(textFields[i], i);
 		}
-
-		btn.setOnAction(al -> {
-			System.out.println(charTranslations[3]);
-		});
 	}
 
 	/**
@@ -170,11 +176,126 @@ public class CryptoquipUI extends Application
 						letter -= ('a' - 'A');
 					if(letter < 'A' || letter > 'Z')
 						letter = '\0';
-					else {} // TODO Check if value already chosen
+					else
+					{
+						for(int i = 0; i < MAX_LETTERS; i++)
+						{
+							if(charTranslations[i] == letter)
+							{
+								charTranslations[i] = '\0';
+								textFields[i].setText("");
+								updateStrings((char)('A' + i), '\0');
+								break;
+							}
+						}
+					}
 					field.setText("" + letter);
 				}
 				charTranslations[index] = letter;
+				updateStrings((char)('A' + index), letter);
 			}
 		});
 	}
+
+	/**
+	 * Sets the puzzle code to solve
+	 * @param code The encrypted code to solve
+	 */
+	private void setPuzzle(String code)
+	{
+		// Set the solution
+		if(code.isEmpty())
+			throw new RuntimeException("Code needs to be a word!");
+		solutionStr = code.toUpperCase();
+		String[] lblStrings = splitString(solutionStr);
+
+		// Set up the labels
+		translatedLabels = new Label[lblStrings.length];
+		codeLabels = new Label[lblStrings.length];
+
+		// Add all labels
+		for(int i = 0; i < lblStrings.length; i++)
+		{
+			String lblCodeStr = "";
+			String lblTranslatedStr = "";
+			for(char c : lblStrings[i].toCharArray())
+			{
+				lblCodeStr += "" + c + " ";
+				lblTranslatedStr += (c >= 'A' && c <= 'Z' ? "_" : "" + c) + " ";
+			}
+			lblCodeStr = lblCodeStr.substring(0, lblCodeStr.length() - 1);
+			lblTranslatedStr = lblTranslatedStr.substring(0, lblTranslatedStr.length() - 1);
+
+			for(int ii = 0; ii < 2; ii++)
+			{
+				Label lbl = new Label(ii == 0 ? lblCodeStr : lblTranslatedStr);
+				lbl.setFont(Font.font("Ubuntu Mono", 30));
+				if(ii == 0)
+					lbl.setStyle("-fx-text-fill: gray");
+				grdStrings.addRow((i * 3) + ii, lbl);
+				GridPane.setHalignment(lbl, HPos.CENTER);
+
+				if(ii == 0)
+					codeLabels[i] = lbl;
+				else
+					translatedLabels[i] = lbl;
+			}
+			grdStrings.addRow((i * 3) + 2, new Label());
+		}
+	}
+
+	/**
+	 * Splits the string to wrap text
+	 * @param strOrig The original string to split
+	 * @return Array of strings, similar to wrapped text
+	 */
+	private String[] splitString(String strOrig)
+	{
+		ArrayList<String> res = new ArrayList<String>();
+
+		// Set up the string lines
+		String[] splitStrs = strOrig.split(" ");
+		if(splitStrs[0].length() > MAX_LABEL_LENGTH)
+			throw new RuntimeException("Error! Word too big: \"" + splitStrs[0] + "\"\nWords must be 20 letters or shorter");
+		res.add(splitStrs[0]);
+
+		// Loop through all strings
+		for(int i = 1; i < splitStrs.length; i++)
+		{
+			String str = splitStrs[i];
+			if(str.length() > MAX_LABEL_LENGTH)
+				throw new RuntimeException("Error! Word too big: \"" + splitStrs[i] + "\"\nWords must be 20 letters or shorter");
+			String currStr = res.get(res.size()-1);
+			if(currStr.length() + 1 + str.length() <= MAX_LABEL_LENGTH) // Check if adding the word will go over the limit (add 1 for ' ')
+				res.set(res.size()-1, currStr + " " + str);
+			else // If too long, add new line
+				res.add(str);
+		}
+		return Arrays.copyOf(res.toArray(), res.size(), String[].class);
+	}
+
+	/**
+	 * Updates the string with a new character
+	 * @param oldChar The old char to switch with the new char
+	 * @param newChar The new character
+	 */
+	private void updateStrings(char oldChar, char newChar)
+	{
+		for(int i = 0; i < codeLabels.length; i++)
+		{
+			// Replace all instances of the old char with the new char
+			String codeStr = codeLabels[i].getText();
+			char[] translatedChars = translatedLabels[i].getText().toCharArray();
+			int index = codeStr.indexOf(oldChar);
+			while(index != -1)
+			{
+				translatedChars[index] = (newChar == '\0' ? '_' : newChar);
+				index = codeStr.indexOf(oldChar, index + 1);
+			}
+			translatedLabels[i].setText(String.valueOf(translatedChars));
+		}
+	}
 }
+
+
+
